e174f3f46a7b6693501461f82b3b59f360553f58
Mahii310321 <maheshmahi23798@gmail.com>
2024-05-21 00:07:37 +0530

    node basics

---
+++ index.js
@@ -0,0 +1,9 @@
+const Person = require("./person");
+
+console.log("Running");
+
+// console.log(__dirname)
+// console.log(__filename)
+const person= new Person('John',10)
+person.fun()
+console.log(person);
